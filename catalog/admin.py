e67b892180cd4cc6978702995779ebdca7b8ccbc
Alexei Voropaev <chaos.73@yandex.ru>
2023-11-18 23:59:08 +0400

    ADD: homework 20.1

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,6 +5,7 @@ from catalog.models import Category, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
+    search_fields = ('title', 'description')
 
 
 @admin.register(Product)
