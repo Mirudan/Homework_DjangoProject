6ebf1ae355d32a258dcfd2420a6f2e2c5059797e
Alexei Voropaev <chaos.73@yandex.ru>
2023-11-26 18:06:46 +0400

    ADD: homework 20.2

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Contact
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
-    search_fields = ('title', 'description')
 
 
 @admin.register(Product)
@@ -13,3 +12,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category')
     list_filter = ('category',)
     search_fields = ('name', 'description')
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+    search_fields = ('name', 'address')
