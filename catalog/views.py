4705c2063766223d91e0f1f80a3da0606a521df2
Alexei Voropaev <chaos.73@yandex.ru>
2023-12-03 23:03:08 +0400

    ADD: homework 21.1

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.views.generic import ListView, DetailView
+
 from catalog.models import Product, Contact
 
 
@@ -15,32 +17,29 @@ def index(request):
     return render(request, 'catalog/index.html', context)
 
 
-def show_item(request, product_pk):
-    item = get_object_or_404(Product, pk=product_pk)
-    context = {
-        'item': item,
-        'title': item
-    }
-    return render(request, 'catalog/item.html', context)
+class ProductList(ListView):
+    model = Product
+    template_name = 'catalog/index.html'
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'{name} ({email}): {message}')
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'catalog/item.html'
 
-    contact_list = [{
-        'name': contact.name,
-        'phone': contact.phone,
-        'address': contact.address,
-        'email': contact.email
-    } for contact in Contact.objects.all()]
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(id=self.kwargs.get('pk'))
+        return queryset
 
-    context = {
-        'title': 'Контакты',
-        'contacts': contact_list
-    }
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
 
-    return render(request, "catalog/contacts.html", context)
+        item = Product.objects.get(pk=self.kwargs.get('pk'))
+        context_data['item'] = item
+        context_data['title'] = item
+        return context_data
+
+
+class ContactsView(ListView):
+    model = Contact
+    template_name = 'catalog/contacts.html'
