f8bbdaffc86515e7d091b54e17f51a7121739fc0
Alexei Voropaev <chaos.73@yandex.ru>
2023-12-05 20:55:59 +0400

    ADD: homework 21.1

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from django.urls import reverse_lazy, reverse
+from django.utils.text import slugify
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from blog.models import Article
@@ -17,9 +18,10 @@ class ArticleUpdateView(UpdateView):
     success_url = reverse_lazy('blog:view_article')
 
     def form_valid(self, form):
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('blog:view_article', args=[self.kwargs.get('slug')]))
+        instance = form.save(commit=True)
+        instance.slug = slugify(instance.title)
+        instance.save()
+        return redirect(reverse('blog:view_article', args=[instance.slug]))
 
 
 class ArticleListView(ListView):
